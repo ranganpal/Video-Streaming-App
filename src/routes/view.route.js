a2df73fa812e8368c3b37de3d49f0658fcd2e69f
ranganpal <ranganpal5@gmail.com>
2024-08-03 09:40:38 +0530

    minor change in view route

--- src/routes/view.route.js
+++ src/routes/view.route.js
@@ -9,13 +9,6 @@ import {
 const router = Router()
 router.use(verifyJWT)
 
-/*
-
-  getVideoViewers
-  getWatchedVideos
-
-*/
-
 router.route("/watched-videos").get(getWatchedVideos)
 router.route("/video-viewers/:videoId").get(getVideoViewers)
 router.route("/remove-from-histroy/:videoId").patch(updateWatchHistory)
