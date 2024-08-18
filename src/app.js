4b9f022208d0b30417ffcf82b90339c06ca56502
ranganpal <ranganpal5@gmail.com>
2024-08-18 15:23:43 +0530

    minor update in app.js

--- src/app.js
+++ src/app.js
@@ -14,11 +14,12 @@ app.use(cookieParser())
 // Router
 import userRouter from "./routes/user.router.js"
 import subscriptionRouter from "./routes/subscription.route.js"
+import videoRouter from "./routes/video.route.js"
 import viewRouter from "./routes/view.route.js"
 
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/subscriptions", subscriptionRouter)
-app.use("/api/v1/videos", subscriptionRouter)
+app.use("/api/v1/videos", videoRouter)
 app.use("/api/v1/views", viewRouter)
 
 
